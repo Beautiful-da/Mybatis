e3ce6fdbfa724505dd12cdcd7abb82b5fcf83682
Da <Da@Beautiful.com>
2022-04-26 21:01:07 +0800

    first commit

---
+++ Git_Test/src/main/java/gitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:Beautiful_da
+ * @Date:2022/4/26 20:58
+ * @Descripiton:
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
